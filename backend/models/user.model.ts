6e34ec5c5b88ebde075c53b69b5d290fcfc3c4b3
elite-coder669 <23071A3233@vnrvjiet.in>
2025-04-22 21:24:21 +0530

    Backend Commit

--- backend/models/user.model.ts
+++ backend/models/user.model.ts
@@ -1,13 +1,16 @@
 import { Schema, model } from "mongoose";
 
 const userSchema = new Schema({
-    name: String,
-    email: String,
-    employeeId: String,
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["admin", "user"], default: "user" },
+    employeeId: { type: String, required: true },
     publications: [{
-        type: Schema.Types.ObjectId,
-        ref: "Publication"
+      type: Schema.Types.ObjectId,
+      ref: "Publication",
+      required: false,
     }]
-})
+  });
 
-const User = model("User", userSchema)
+export const User = model("User", userSchema)
